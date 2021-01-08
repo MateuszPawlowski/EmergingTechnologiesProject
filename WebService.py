059e8503651476db97bdc6c0a6e75ecca4387aad
Mateusz Pawlowski <g00361162@gmit.ie>
2021-01-08 19:18:38 +0000

    Final Submission

--- WebService.py
+++ WebService.py
@@ -1,28 +1,27 @@
 # imports
-from flask import Flask, jsonify, request
+from flask import Flask, request
 import json
 import tensorflow as tf
 app = Flask(__name__)
 
-# getting the home.html 
+# getting the home.html
 @app.route('/')
 def getWeb():
     return app.send_static_file('home.html')
 
 # getting the power and sending it
+# request the data
+# load the data using json
+# get the speed
+# load powerproduction from model
+# get predictions
+# turn it to a 1 dimensional array using flatten and return it
 @app.route('/calculate/power', methods=['POST'])
 def powerPrediction():
-    # request the data
     data = request.data
-    # load the data using json
     jsonLoad = json.loads(data)
-    # get the speed
     windSpeed = float(jsonLoad['speed'])
-    # load powerproduction from model
     model = tf.keras.models.load_model('powerProduction.h5')
-    # get predictions
     prediction = model.predict([windSpeed])
-    # turn it to a 1 dimensional array using flatten and return it
-    flatten = prediction.flatten()
-    print(flatten[0])
-    return str(flatten[0])
+    flattenTo1D = prediction.flatten()
+    return str(flattenTo1D[0])
